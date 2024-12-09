aee39101208da18b9b188356b663136bdf66bddc
Lavon Sykes <lavonsykes2003@gmail.com>
2024-12-09 09:58:50 -0500

    finished CGN experiments

--- CGN.py
+++ CGN.py
@@ -14,7 +14,7 @@ def solve_cgn(A, b, num_iter = int(1e5), x_0 = None, tol = 1e-12):
         tol (optional) : residual tolerance for approximating convergence 
 
         OUTPUT: 
-        x : (N x 1) vector solution to Ax = b
+        x : (N x 1) vector solution to Ax = b and A* Ax = A* b (multiplication by conjugate to achieve PSD symmetric matrix A* A)
         res : vector of residual norms 
         path : trajectory of steps from `x_0` to x_final
         conv : boolean indicating convergence within `num_iter` iterations
@@ -24,26 +24,28 @@ def solve_cgn(A, b, num_iter = int(1e5), x_0 = None, tol = 1e-12):
     '''
 
     A = np.matrix(A)
-    x_0 = np.zeros(shape = (np.shape(A)[1] , ))
+    x_0 = np.zeros((A.shape[1] , ))  # initial random guess on the unit hypersphere
 
     beta = [0]
     p = [np.zeros_like(b)]
-    r = [1e5 * np.ones(shape = (np.shape(A)[0], ))]
+    r = [np.array(np.array(b) - A @ x_0).reshape(-1, )]
     alpha = []
     x = [x_0]
     t = 0
+    print(r[-1], np.array(r[-1]))
 
-    while np.linalg.norm(np.array(r[-1]).reshape(len(r[-1]), )) >= tol:   
+
+    while np.linalg.norm(np.array(r[-1]).reshape(-1, )) >= tol:   
         if t >= num_iter:
             break
-        p.append(np.array(A.H @ (np.array(r[-1]).reshape(len(r[-1]), )) + beta[-1] * (np.array(p[-1]).reshape(len(p[-1]), ))).reshape(p[0].shape[0], ))
-        alpha.append(np.linalg.norm(A.H @ np.array(r[-1]).reshape(len(r[-1]), ))**2 / np.linalg.norm(A @ np.array(p[-1]).reshape(len(p[-1]), ))**2)
-        x.append(np.array(x[-1]).reshape(len(x[-1]), ) + alpha[-1] * np.array(p[-1]).reshape(len(p[-1]), ))
-        r.append(np.array(np.array(r[-1]).reshape(len(r[-1]), ) - alpha[-1] * (A @ np.array(p[-1]).reshape(len(p[-1]), ))).reshape(r[0].shape[0], ))
-        beta.append(np.linalg.norm(A.H @ np.array(r[-1]).reshape(len(r[-1]), ))**2 / np.linalg.norm(A @ np.array(r[-2]).reshape(len(r[-2]), ))**2)
+        p.append(np.array(A.H @ (np.array(r[-1]).reshape(-1, )) + beta[-1] * (np.array(p[-1]).reshape(-1, ))).reshape(p[0].shape[0], ))
+        alpha.append(np.linalg.norm(A.H @ np.array(r[-1]).reshape(-1, ))**2 / np.linalg.norm(A @ np.array(p[-1]).reshape(-1, ))**2)
+        x.append(np.array(x[-1]).reshape(-1, ) + alpha[-1] * np.array(p[-1]).reshape(-1, ))
+        r.append(np.array(np.array(r[-1]).reshape(-1, ) - alpha[-1] * (A @ np.array(p[-1]).reshape(-1, ))).reshape(r[0].shape[0], ))
+        beta.append(np.linalg.norm(A.H @ np.array(r[-1]).reshape(-1, ))**2 / np.linalg.norm(A @ np.array(r[-2]).reshape(-1, ))**2)
 
         if t % 100 == 0:
-            print(f'Iteration: {t} \t Error: {np.linalg.norm(np.array(r[-1]).reshape(len(r[-1]), ))}')
+            print(f'Iteration: {t} \t Error: {np.linalg.norm(np.array(r[-1]).reshape(-1, ))}')
         if t % 500 == 0:
             clear_output(wait = True)
         t += 1
